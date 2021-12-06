6d644eae1dd46f246ef1523e97444e5a1fabc0f5
GabrielSouza96 <gabriel.souza_01@hotmail.com>
2021-12-06 11:59:42 -0300

    Seeding Service

--- WEBMVC/Startup.cs
+++ WEBMVC/Startup.cs
@@ -38,14 +38,18 @@ namespace WEBMVC
 
             services.AddDbContext<WEBMVCContext>(options =>
                     options.UseMySql(Configuration.GetConnectionString("WEBMVCContext"), builder => builder.MigrationsAssembly("WEBMVC")));
+
+            services.AddScoped<SeedingService>();
+        
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                seedingService.Seed();
             }
             else
             {
