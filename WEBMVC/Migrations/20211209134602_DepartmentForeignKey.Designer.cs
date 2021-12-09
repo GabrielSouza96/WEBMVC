ced4eb1e5a6dc9c1a6f166d6759268c71d33dd8b
GabrielSouza96 <gabriel.souza_01@hotmail.com>
2021-12-09 10:51:33 -0300

    ForeignKey not null

--- WEBMVC/Migrations/20211209134602_DepartmentForeignKey.Designer.cs
+++ WEBMVC/Migrations/20211209134602_DepartmentForeignKey.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WEBMVC.Data;
 
 namespace WEBMVC.Migrations
 {
     [DbContext(typeof(WEBMVCContext))]
-    partial class WEBMVCContextModelSnapshot : ModelSnapshot
+    [Migration("20211209134602_DepartmentForeignKey")]
+    partial class DepartmentForeignKey
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
