64435acb5143c36cc1ec3a2fa31bb25f0a59de2a
GabrielSouza96 <gabriel.souza_01@hotmail.com>
2021-12-13 11:08:09 -0300

    Seller details and eager loading

--- WEBMVC/Services/SellerService.cs
+++ WEBMVC/Services/SellerService.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using WEBMVC.Models;
 using WEBMVC.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace WEBMVC.Services
 {
@@ -30,7 +31,7 @@ namespace WEBMVC.Services
 
         public Seller FindById(int id)
         {
-            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+            return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
         }
 
         public void Remove(int id)
